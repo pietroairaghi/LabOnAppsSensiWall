9347644e222befa03e0514c835b4c9ebc354c511
jacoposwald <jacopo.oswald@gmail.com>
2018-11-14 11:57:53 +0100

    Onclick + TestActivity

--- mobile/src/main/java/project/labonappssensiwall/SessionActivity.java
+++ mobile/src/main/java/project/labonappssensiwall/SessionActivity.java
@@ -3,6 +3,7 @@ package project.labonappssensiwall;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class SessionActivity extends AppCompatActivity {
@@ -18,6 +19,24 @@ public class SessionActivity extends AppCompatActivity {
         String sessionName = extras.getString(MainActivity.SESSION_NAME);
 
         TextView session = findViewById(R.id.sessionName);
-        session.setText(sessionName);
+        session.setText("Welcome to " + sessionName + "!");
+    }
+
+    public void clickedbuttonSetting(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+       // intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void clickedbuttonTest(View view) {
+        Intent intent = new Intent(this, TestActivity.class);
+        // intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void clickedbuttonStart(View view) {
+    }
+
+    public void clickedbuttonDraw(View view) {
     }
 }
