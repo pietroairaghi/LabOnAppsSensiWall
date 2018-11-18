fb51d48631af3655ff50909525f0c48590007b4a
jacoposwald <jacopo.oswald@gmail.com>
2018-11-18 12:37:29 +0100

    New Session

--- mobile/src/main/java/project/labonappssensiwall/SessionActivity.java
+++ mobile/src/main/java/project/labonappssensiwall/SessionActivity.java
@@ -1,12 +1,29 @@
 package project.labonappssensiwall;
 
 import android.content.Intent;
+import android.media.MediaCodec;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SessionActivity extends AppCompatActivity {
+
+    private static final String TAG = "SessionActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +34,29 @@ public class SessionActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         String sessionName = extras.getString(MainActivity.SESSION_NAME);
+        String sessionID = extras.getString(MainActivity.SESSION_ID);
+
+        if(sessionID.equals("newSession")) {
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+            Map<String, Object> newSession = new HashMap<>();
+            newSession.put("name", "New Session");
+
+            db.collection("sessions").document()
+                    .set(newSession)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d(TAG, "DocumentSnapshot successfully written!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error writing document", e);
+                        }
+                    });
+        }
 
         TextView session = findViewById(R.id.sessionName);
         session.setText("Welcome to " + sessionName + "!");
